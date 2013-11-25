907934beed87d5c446fe0d8668f5ef2776c0c068
Gregory Norton <gregnorton@gmail.com>
2013-11-25 14:13:10 -0800

    GroundEntity has populateMap() static method.
    PlayingState calls populateMap with the TiledMap object.

--- atlantis/src/atlantis/GroundEntity.java
+++ atlantis/src/atlantis/GroundEntity.java
@@ -3,6 +3,8 @@ package atlantis;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.newdawn.slick.tiled.TiledMap;
+
 import jig.Vector;
 import dijkstra.engine.DijkstraAlgorithm;
 import dijkstra.model.Edge;
@@ -24,9 +26,10 @@ abstract class GroundEntity extends AtlantisEntity {
 
 	/* -------------------------------------------------------------------- */
 
-	/* Ground entities have to move according to the map. */
+	/* Ground entities have to move according to the map. Populate from 
+	 * TiledMap data. */
 	
-	static {
+	public static void populateTerrainMap(TiledMap map) {
 		List<Edge> map_edges = new ArrayList<Edge>();
 		List<Vertex> map_nodes = new ArrayList<Vertex>();
 
