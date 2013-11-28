577913c2d77154e5313e5ded6492aea1f45a595b
Elijah Houle <elijah.houle@gmail.com>
2013-11-27 18:11:50 -0800

    Add unit selecting/highlighting and movement command. Need to pull fix to Dijkstra's before continuing.

--- atlantis/src/atlantis/Overlay.java
+++ atlantis/src/atlantis/Overlay.java
@@ -1,5 +1,7 @@
 package atlantis;
 
+import java.util.List;
+
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -9,6 +11,7 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
 import jig.ResourceManager;
+import jig.Vector;
 
 public class Overlay {
 	Image overlay;
@@ -20,9 +23,13 @@ public class Overlay {
 	
 	boolean isDefaultCursorSet = true;
 	
-	short action = 0; // 1 = move, 2 = attack
+	PlayingState playingState;
+	public AtlantisEntity selectedUnit = null;
+	public short action = 0; // 1 = move, 2 = attack
 	
-	public Overlay() {
+	public Overlay(PlayingState ps) {
+		playingState = ps;
+		
 		// TODO: model of unit on left, minimap on right
 		overlay = ResourceManager.getImage(AtlantisGame.OVERLAY);
 		actionMove = ResourceManager.getImage(AtlantisGame.ACTION_MOVE);
@@ -40,10 +47,36 @@ public class Overlay {
 		int x = input.getMouseX();
 		int y = input.getMouseY();
 		
+		if (selectedUnit != null) { // highlight selected unit
+			g.setColor(Color.yellow);
+			g.drawRect(selectedUnit.getCoarseGrainedMinX(),
+					selectedUnit.getCoarseGrainedMinY(),
+					selectedUnit.getCoarseGrainedWidth(),
+					selectedUnit.getCoarseGrainedHeight());
+		}
+		
 		if (y > 470) { // select action
 			if (!isDefaultCursorSet) {
 				container.setMouseCursor(AtlantisGame.cursor, 0, 0);
 				isDefaultCursorSet = true;
+			}
+		} else if (action == 0) { // select unit
+			if (!isDefaultCursorSet) {
+				container.setMouseCursor(AtlantisGame.cursor, 0, 0);
+				isDefaultCursorSet = true;
+			}
+			
+			if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
+				List<Worker> workers = playingState.getStatus().getWorkers();
+				for (Worker worker : workers) {
+					if (y > worker.getCoarseGrainedMinY() &&
+							y < worker.getCoarseGrainedMaxY() &&
+							x > worker.getCoarseGrainedMinX() &&
+							x < worker.getCoarseGrainedMaxX()) {
+						selectedUnit = worker;
+						break;
+					}
+				}
 			}
 		} else if (action == 1) { // move
 			if (isDefaultCursorSet) {
@@ -51,6 +84,13 @@ public class Overlay {
 				isDefaultCursorSet = false;
 			}
 			g.drawImage(targetMove, x, y);
+			
+			if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON) ||
+					input.isMouseButtonDown(Input.MOUSE_RIGHT_BUTTON)) {
+				System.out.println(selectedUnit.moveTo(new Vector(x, y)));
+				action = 0;
+				selectedUnit = null;
+			}
 		} else if (action == 2) { // attack
 			if (isDefaultCursorSet) {
 				container.setMouseCursor(pixel, 0, 0);
@@ -60,10 +100,12 @@ public class Overlay {
 		}
 		
 		g.drawImage(overlay, 0, 470);
-		g.drawImage(actionMove, 290, 520);
-		g.drawImage(actionAttack, 350, 520);
+		if (selectedUnit != null) {
+			g.drawImage(actionMove, 290, 520);
+			g.drawImage(actionAttack, 350, 520);
+		}
 		
-		if (y > 520 && y < 570) {
+		if (y > 520 && y < 570 && selectedUnit != null) {
 			if (x > 290 && x < 340) { // move button
 				// tooltip
 				x += 20;
@@ -72,7 +114,7 @@ public class Overlay {
 				g.setColor(Color.black);
 				g.drawString("Move", x, y);
 				
-				if (input.isMousePressed(Input.MOUSE_LEFT_BUTTON)) action = 1;
+				if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) action = 1;
 			} else if (x > 350 && x < 400) { // attack button
 				// tooltip
 				x += 20;
@@ -81,7 +123,7 @@ public class Overlay {
 				g.setColor(Color.black);
 				g.drawString("Attack", x, y);
 				
-				if (input.isMousePressed(Input.MOUSE_LEFT_BUTTON)) action = 2;
+				if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) action = 2;
 			}
 		}
 	}
